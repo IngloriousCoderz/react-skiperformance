9b74c1d25acb5b9ee2e6cd534f9139587364fa38
Matteo Antony Mistretta <m.mistretta@reply.it>
2024-02-23 17:35:47 +0100

    Complete the counter reducer example

--- 2-todo-list/src/08-redux/1-counter/counter.reducer.test.ts
+++ 2-todo-list/src/08-redux/1-counter/counter.reducer.test.ts
@@ -1,14 +1,66 @@
 import { test, expect } from "vitest";
 
-import { increment } from "./counter.reducer";
+import counter, { increment, reset } from "./counter.reducer";
 
 test("it should increment by 1", () => {
   // given
   const stateBefore = 0;
+  const action = increment();
   const stateAfter = 1;
 
   // when
-  const state = increment(stateBefore);
+  const state = counter(stateBefore, action);
+
+  // then
+  expect(state).toBe(stateAfter);
+});
+
+test("it should increment by a given amount", () => {
+  // given
+  const stateBefore = 0;
+  const action = increment(5);
+  const stateAfter = 5;
+
+  // when
+  const state = counter(stateBefore, action);
+
+  // then
+  expect(state).toBe(stateAfter);
+});
+
+test("it should reset", () => {
+  // given
+  const stateBefore = 7;
+  const action = reset();
+  const stateAfter = 0;
+
+  // when
+  const state = counter(stateBefore, action);
+
+  // then
+  expect(state).toBe(stateAfter);
+});
+
+test("it should ignore any other action", () => {
+  // given
+  const stateBefore = 2;
+  const action = { type: "make coffee" };
+
+  // when
+  const state = counter(stateBefore, action);
+
+  // then
+  expect(state).toBe(stateBefore);
+});
+
+test("it should initialize the state", () => {
+  // given
+  const stateBefore = undefined;
+  const action = { type: "make coffee" };
+  const stateAfter = 0;
+
+  // when
+  const state = counter(stateBefore, action);
 
   // then
   expect(state).toBe(stateAfter);
--- 2-todo-list/src/08-redux/1-counter/counter.reducer.ts
+++ 2-todo-list/src/08-redux/1-counter/counter.reducer.ts
@@ -1,5 +1,38 @@
+const INITIAL_STATE = 0;
 const DEFAULT_INCREMENT = 1;
 
-export function increment(state: number) {
-  return state + DEFAULT_INCREMENT;
+type TAction = {
+  type: string;
+  payload?: number;
+};
+
+// action types
+const INCREMENT = "increment";
+const RESET = "reset";
+
+// action creators
+export const increment = (amount?: number) => ({
+  type: INCREMENT,
+  payload: amount,
+});
+export const reset = () => ({ type: RESET });
+
+// reducer
+export default function counter(
+  state: number = INITIAL_STATE,
+  action: TAction
+) {
+  switch (action.type) {
+    case INCREMENT:
+      return state + (action.payload ?? DEFAULT_INCREMENT);
+
+    case RESET:
+      return INITIAL_STATE;
+
+    default:
+      return state;
+  }
 }
+
+// const actions = [increment(2), increment(3), reset(), increment()]
+// const state = actions.reduce(counter, 0)
